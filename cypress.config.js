d50c5b5e419cc5c0a9aff516dfb4cffd0758656a
Luis Benitez <luisarmandobr@gmail.com>
2023-08-31 04:26:30 -0500

    Rollback Cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,5 @@
-const { defineConfig } = require("cypress");
-import { defineConfig } from "cypress";
-import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
-import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
-    specPattern: "**/*.feature",
-    async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config);
-      on("file:preprocessor", createBundler({
-        plugins: [createEsbuildPlugin(config)],
-      }));
-      return config;
-    },
-
+    setupNodeEvents(on, config) {},
   },
-});
+}
